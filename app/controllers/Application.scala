1a19aa40a8bddb170ab23f0ee155b1b6064b5c0f
pamu <nagarjuna.pamu@gmail.com>
2015-03-26 12:09:35 +0530

    case classes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,26 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
+  
   def index = Action {
     Ok(views.html.index("BigHassle"))
   }
+
+  case class FoodInfo(description: String, special: Option[String] = None)
+  case class SlotInfo(dayNo: Int, slotNo: Int, name: String, startTime: String, span: Int, foodInfo: FoodInfo)
+  case class DayInfo(slots: List[SlotInfo])
+  case class MenuInfo(days: List[DayInfo])
+  
+  def slot(day: Int, slot: Int) = Action(parse.json) { implicit request =>
+    Ok("")
+  }
+
+  def day(day: Int) = Action(parse.json) { implicit request =>
+    Ok("")
+  }
+  
+  def menu() = Action(parse.json) { implicit request =>
+    Ok("")
+  }
+
 }
