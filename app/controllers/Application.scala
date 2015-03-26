75045ee4fa0776b85f61e2aa7f6442ebee95767c
pamu <nagarjuna.pamu@gmail.com>
2015-03-26 14:50:32 +0530

    json writes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 
 object Application extends Controller {
   
@@ -9,14 +12,56 @@ object Application extends Controller {
   }
 
   case class FoodInfo(description: String, special: Option[String] = None)
-  case class SlotInfo(dayNo: Int, slotNo: Int, name: String, startTime: String, span: Int, foodInfo: FoodInfo)
+  case class SlotInfo(dayNo: Int, slotNo: Int, name: String, startTime: String, span: Int, 
+      foodInfo: FoodInfo)
   case class DayInfo(slots: List[SlotInfo])
   case class MenuInfo(days: List[DayInfo])
   
   implicit val foodInfoReads: Reads[FoodInfo] = (
+      (JsPath \ "description").read[String] and
+      (JsPath \ "special").read[Option[String]] 
+  )(FoodInfo.apply _)
+  
+  implicit val slotInfoReads: Reads[SlotInfo] = (
+      (JsPath \ "dayNo").read[Int] and
+      (JsPath \ "slotNo").read[Int] and
+      (JsPath \ "name").read[String] and
+      (JsPath \ "startTime").read[String] and
+      (JsPath \ "span").read[Int] and
+      (JsPath \ "foodInfo").read[FoodInfo]
+  )(SlotInfo.apply _)
+  
+  implicit val dayInfoReads: Reads[DayInfo] = (
+      (JsPath \ "slots").read[List[SlotInfo]].map(DayInfo(_))
   )
+  
+  implicit val menuInfoReads: Reads[MenuInfo] = (
+      (JsPath \ "days").read[List[DayInfo]].map(MenuInfo(_))
+  )
+  
+  implicit val foodInfoWrites: Writes[FoodInfo] = (
+      (JsPath \ "description").write[String] and
+      (JsPath \ "special").write[Option[String]]
+  )(unlift(FoodInfo.unapply))
+  
+  implicit val slotInfoWrites: Writes[SlotInfo] = (
+      (JsPath \ "dayNo").write[Int] and
+      (JsPath \ "slotNo").write[Int] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "startTime").write[String] and
+      (JsPath \ "span").write[Int] and
+      (JsPath \ "foodInfo").write[FoodInfo]
+  )(unlift(SlotInfo.unapply))
+  
+  implicit val dayInfoWrites: Writes[DayInfo] = (
+      (JsPath \ "slots").write[List[SlotInfo]].contramap[DayInfo](_.slots)
+  )
+  
+  implicit val menuInfoWrites: Writes[MenuInfo] = (
+     (JsPath \ "days").write[List[DayInfo]].contramap[MenuInfo](_.days)
+  )
+  
   def slot(day: Int, slot: Int) = Action(parse.json) { implicit request =>
-    
     Ok("")
   }
 
